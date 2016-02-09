ae71efb9744d3f2f169a29d1fdfe8ffac489b3bf
Zarthak <gamer126@hotmail.fr>
2016-02-09 18:14:39 +0100

    Interface

--- src/main/java/fr/univ_fcomte/iutbm/views/Accueil.java
+++ src/main/java/fr/univ_fcomte/iutbm/views/Accueil.java
@@ -1,12 +1,17 @@
 package fr.univ_fcomte.iutbm.views;
 
+import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.Panel;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.JTextField;
 
 public class Accueil extends JFrame{
@@ -14,22 +19,43 @@ public class Accueil extends JFrame{
 	
 	private JPanel buildContentPane(){
 		JPanel jPanel = new JPanel();
-		jPanel.setLayout(new FlowLayout());
-		Object[] element = new Object[]{"domaine1","domaine2"};
+		JPanel jPanel2 = new JPanel();
+		jPanel.setLayout(new BorderLayout());
+		
+		JPanel jPanel3 = new JPanel();
+		jPanel3.setLayout(new BorderLayout());
+		
+		JPanel jPanel4 = new JPanel();
+		JLabel jLabel1 = new JLabel("Categories");
+		JTextField jTextField = new JTextField("",20);
+		JLabel jLabel2 = new JLabel("Mot clés");
+		JTextField jTextField2 = new JTextField("",20);
+		
+		jPanel4.add(jLabel1);
+		jPanel4.add(jTextField);
+		jPanel4.add(jLabel2);
+		jPanel4.add(jTextField2);
+		Object[] element = new Object[]{""};
 		listeDomaine= new JComboBox(element);
-		JButton jb = new JButton("Ajouter un domaine");
-		//JTextField jTextField= new JTextField("Mot clés");
-		//JTextField jTextField2= new JTextField("Catégories");
-		jPanel.add(listeDomaine);
-		jPanel.add(jb);
-		//jPanel.add(jTextField);
-		//jPanel.add(jTextField2);
+		listeDomaine.setPrototypeDisplayValue("Nom du domaine");
+		JButton jb = new JButton("Ajouter un domaine");		
+		jPanel2.add(listeDomaine);
+		jPanel2.add(jb);
+		jPanel.add(jPanel2,BorderLayout.NORTH);
+		Object[][] data = {};
+	    String  title[] = {"Mot clés", "Critères"};
+	    JTable tableau = new JTable(data, title);
+	    JList doc = new JList(data);
+	    jPanel.add((new JScrollPane(tableau)),BorderLayout.CENTER);
+	    jPanel3.add(jPanel4,BorderLayout.NORTH);
+	    jPanel3.add((new JScrollPane(doc)),BorderLayout.CENTER);
+	    jPanel.add(jPanel3,BorderLayout.SOUTH);
 		return jPanel;
 	}
 	
 	public Accueil(){
 		setContentPane(buildContentPane());
-		setSize(200,200);
+		setSize(1600,800);
 		setVisible(true);
 	}
 }
